f2803552dbb27f5f0a41165aa89ef46e5bfed41d
Luiz Gustavo Banin De Sena <luizgbs1@gmail.com>
2021-02-12 15:25:56 -0200

    Vertical construção

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,13 +10,68 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string[,] velha = new string[3, 3]; // Matriz jogo da velha
-            int jogador = 0;  // X = 0    O = 1
+            string[,] velha = new string[3, 3];
+            int Quem_Comeca;
+            bool jogador1 = false;
+
+            //Controlando a entrada do usuario
+            Console.WriteLine("O primeiro jogador sempre começa como X e o segundo como O");
+            do
+            {
+                Console.WriteLine("Informe quem vai começar (jogador 1 digite 1 , jogador 2 digite 2) : ");
+                Quem_Comeca = int.Parse(Console.ReadLine());
+            } while (Quem_Comeca != 1 && Quem_Comeca != 2);
+
+            if (Quem_Comeca == 1)
+            {
+                jogador1 = true;
+                Console.WriteLine("---------JOGADOR 1 COMEÇA---------\n");
+            }
+            else
+            {
+                Console.WriteLine("---------JOGADOR 2 COMEÇA---------\n");
+            }
+
+            Console.WriteLine("As posições do jogo são essas:\n");
 
             iniciamatriz(velha);
-
             imprime(velha);
 
+            Console.WriteLine("");
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (jogador1)
+                {
+                    Console.WriteLine("Jogador 1, sua vez de jogar: ");
+                }
+                else
+                {
+                    Console.WriteLine("Jogador 2, sua vez de jogar: ");
+                }
+
+                solicita_posicao(velha, jogador1);
+
+
+                imprime(velha);
+
+                if (verificastatus(velha) == 1) {
+                    Console.WriteLine("Jogador 1 ganhou");
+                    break;
+                }
+                else if (verificastatus(velha) == 2) {
+                    Console.WriteLine("Jogador 2 ganhou");
+                    break;
+                }
+                if (!jogador1)
+                {
+                    jogador1 = true;
+                }
+                else
+                {
+                    jogador1 = false;
+                }
+            }
 
             Console.ReadKey();
         }
@@ -27,6 +82,39 @@ namespace ConsoleApp1
                 for (int y = 0; y < matriz.GetLength(1); y++)
                     matriz[i, y] = i.ToString() + "," + y.ToString();
         }
+
+        static void solicita_posicao(string[,] matriz, bool jogador1)
+        {
+            int linha;
+            int coluna;
+
+            do
+            {
+                do
+                {
+                    Console.Write("Digite a linha: ");
+                    linha = int.Parse(Console.ReadLine());
+
+                    Console.Write("Digite a coluna: ");
+                    coluna = int.Parse(Console.ReadLine());
+                    if (linha > (matriz.GetLength(0) - 1) || (coluna > matriz.GetLength(1) - 1))
+                        Console.WriteLine("DIGITE UM VALOR DENTRO DA MATRIZ");
+                } while (linha > (matriz.GetLength(0) - 1) || (coluna > matriz.GetLength(1) - 1));
+                
+
+                if (matriz[linha, coluna] == "X" || matriz[linha, coluna] == "O")
+                   Console.WriteLine("DIGITE UMA POSIÇÃO NÃO USADA");
+            } while (matriz[linha, coluna] == "O" || matriz[linha, coluna] == "X");
+
+            Console.WriteLine("");
+
+            if (jogador1)
+                matriz[linha, coluna] = "X";
+            else
+                matriz[linha, coluna] = "O";
+
+            return;
+        }
         static void imprime(string[,] matriz)
         {
             for(int i = 0; i < matriz.GetLength(0); i++) { 
@@ -35,6 +123,60 @@ namespace ConsoleApp1
                 }
                 Console.WriteLine("");
             }
+        }
+
+        static int verificastatus(string[,] matriz)
+        {
+            string comparar;
+            bool repete = false;
+            //HORIZONTAL
+            
+            for(int i = 0; i < matriz.GetLength(0); i++) { 
+                for(int y = 0; y < 1; y++)
+                {
+                    comparar = matriz[i, y];
+
+                    for (int c = 1; c < matriz.GetLength(1); c++)
+                    {
+                        if(comparar == matriz[i, c])
+                        {
+                            if (repete) {
+                                if (matriz[i, c] == "X")
+                                    return 1;
+                                else
+                                    return 2;
+                            }
+                            repete = true;
+                        }
+                    }
+                }
+            }
+
+            repete = false;
+
+            for (int l = 0; l < 1; l++)
+                for (int c = 0; c < matriz.GetLength(1); c++)
+                {
+                    comparar = matriz[l, c];
+
+                    for (int z = 1; z < matriz.GetLength(1); z++)
+                    {
+                        if (comparar == matriz[z, c])
+                        {
+                            if (repete)
+
+                            {
+                                if (matriz[z, c] == "X")
+                                    return 1;
+                                else
+                                    return 2;
+                            }
+                            repete = true;
+                        }
+                    }
+                }
+            
+            return 0;
         }
     }
 }
