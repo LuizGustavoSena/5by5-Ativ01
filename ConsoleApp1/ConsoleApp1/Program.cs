52f8f9da530d6c951c26c3b700ba57586e402473
LuizGustavoSena <luizgbs1@gmail.com>
2021-02-15 00:57:03 -0300

    Final 0.1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,13 +14,11 @@ namespace ConsoleApp1
             string Quem_Comeca;
             bool jogador1 = false;
 
-            //Controlando a entrada do usuario
-            //Console.WriteLine("O primeiro jogador sempre começa como X e o segundo como O");
             Console.WriteLine("PARA JOGAR VERIFIQUE AS POSIÇÕES DISPONÍVEIS\n" +
                                     "INFORMADA POR (LINHA, COLUNA) NA MATRIZ\n" +
                                     "E INFORME PRIMEIRO A LINHA E DEPOIS A COLUNA\n");
 
-            do
+            do // LOOP VALOR DIFERENTE QUE 1 OU 2 PARA O JOGADOR
             {
                 Console.WriteLine("Informe quem irá começar\n" +
                     "Jogador 1 (X) digite 1\nJogador 2 (O) digite 2");
@@ -28,18 +26,10 @@ namespace ConsoleApp1
 
             } while (Quem_Comeca != "1" && Quem_Comeca != "2");
 
-            Console.Clear();
+            Console.Clear(); // LIMPA TELA DO CONSOLE
 
             if (Quem_Comeca == "1")
-            {
                 jogador1 = true;
-                //Console.WriteLine("---------JOGADOR 1 COMEÇA---------\n");
-            }
-            /*else
-            {
-                Console.WriteLine("---------JOGADOR 2 COMEÇA---------\n");
-            }*/
-
 
             iniciamatriz(velha);
             imprime(velha);
@@ -48,39 +38,34 @@ namespace ConsoleApp1
 
             for (int i = 0; i < 9; i++)
             {
-
                 if (jogador1)
-                {
                     Console.WriteLine("\n---------JOGADOR 1 (X)---------");
-                }
                 else
-                {
                     Console.WriteLine("\n---------JOGADOR 2 (O)---------");
-                }
 
                 solicita_posicao(velha, jogador1);
 
-                Console.Clear();
+                Console.Clear(); // LIMPA TELA DO CONSOLE
 
                 imprime(velha);
 
-                if (verificastatus(velha) == 1) {
+                if (verificastatus(velha) == 1)
+                {
                     Console.WriteLine("Jogador 1 (X) ganhou");
                     break;
                 }
-                else if (verificastatus(velha) == 2) {
+                else if (verificastatus(velha) == 2)
+                {
                     Console.WriteLine("Jogador 2 (O) ganhou");
                     break;
                 }
-                else if(i == 8)
+                else if (i == 8)
                     Console.WriteLine("VELHA");
 
                 if (!jogador1)
                     jogador1 = true;
                 else
                     jogador1 = false;
-
-                
             }
 
             Console.ReadKey();
@@ -99,9 +84,9 @@ namespace ConsoleApp1
             int coluna;
             try
             {
-                do
+                do // LOOP NÚMERO EM UMA POSIÇÃO JÁ UTILIZADA
                 {
-                    do
+                    do // LOOP NÚMERO EM UMA POSIÇÃO MAIOR QUE O TAMANHO DA MATRIZ
                     {
                         Console.Write("Digite a linha: ");
                         linha = int.Parse(Console.ReadLine());
@@ -111,7 +96,6 @@ namespace ConsoleApp1
                         if (linha > (matriz.GetLength(0) - 1) || (coluna > matriz.GetLength(1) - 1) || linha < 0 || coluna < 0)
                             Console.WriteLine("DIGITE UM VALOR DENTRO DA MATRIZ");
                     } while (linha > (matriz.GetLength(0) - 1) || (coluna > matriz.GetLength(1) - 1) || linha < 0 || coluna < 0);
-
 
                     if (matriz[linha, coluna] == " X" || matriz[linha, coluna] == " O")
                         Console.WriteLine("DIGITE UMA POSIÇÃO NÃO USADA");
@@ -130,7 +114,7 @@ namespace ConsoleApp1
                 Console.WriteLine("Digite apenas números existentes na matriz");
                 solicita_posicao(matriz, jogador1);
             }
-            
+
             return;
         }
         static void imprime(string[,] matriz)
@@ -138,15 +122,15 @@ namespace ConsoleApp1
             Console.WriteLine("As posições do jogo são essas:\n");
 
             Console.WriteLine("\t-------------------------");
-            for (int i = 0; i < matriz.GetLength(0); i++) { 
-                for(int y = 0; y < matriz.GetLength(1); y++) {
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int y = 0; y < matriz.GetLength(1); y++)
                     Console.Write("\t|  " + matriz[i, y]);
-                }
-                Console.Write("\t|");
+
+                Console.Write("\t|"); // DEIXAR MATRIZ EM UM FORMATO DE TABULEIRO
                 Console.WriteLine("");
                 Console.Write("\t-------------------------");
                 Console.WriteLine("");
-
             }
         }
 
@@ -155,17 +139,56 @@ namespace ConsoleApp1
             string comparar;
             bool repete = false;
 
-            //HORIZONTAL
-            for(int i = 0; i < matriz.GetLength(0); i++) { 
-                for(int y = 0; y < 1; y++)
+            // VERIFICA DIAGONAL SECUNDÁRIA
+            comparar = matriz[1, 1];
+            for (int i = 0, y = 2; i < 2 * (matriz.GetLength(0) - 1); i += 2, y -= 2)
+            {
+                if (comparar == matriz[i, y])
+                {
+                    if (repete)
+                    {
+                        if (matriz[i, y] == " X")
+                            return 1;
+                        else
+                            return 2;
+                    }
+
+                    repete = true;
+                }
+            }
+
+            repete = false;
+            // VERIFICA DIAGONAL PRINCIPAL
+            comparar = matriz[0, 0];
+            for (int i = 1; i < matriz.GetLength(0); i++)
+            {
+                if (comparar == matriz[i, i])
+                {
+                    if (repete)
+                    {
+                        if (matriz[i, i] == " X")
+                            return 1;
+                        else
+                            return 2;
+                    }
+                    repete = true;
+                }
+            }
+
+            repete = false;
+            // VERIFICA HORIZONTAL
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int y = 0; y < 1; y++)
                 {
                     comparar = matriz[i, y];
 
                     for (int c = 1; c < matriz.GetLength(1); c++)
                     {
-                        if(comparar == matriz[i, c])
+                        if (comparar == matriz[i, c])
                         {
-                            if (repete) {
+                            if (repete)
+                            {
                                 if (matriz[i, c] == " X")
                                     return 1;
                                 else
@@ -179,7 +202,7 @@ namespace ConsoleApp1
             }
 
             repete = false;
-            //VERTICAL
+            // VERIFICA VERTICAL
             for (int l = 0; l < 1; l++)
                 for (int c = 0; c < matriz.GetLength(1); c++)
                 {
@@ -201,42 +224,6 @@ namespace ConsoleApp1
                     }
                     repete = false;
                 }
-
-            //DIAGONAL PRINCIPAL
-            comparar = matriz[0, 0];
-            for (int i = 1; i < matriz.GetLength(0); i++)
-            {
-                if(comparar == matriz[i, i])
-                {
-                    if (repete)
-                    {
-                        if (matriz[i, i] == " X")
-                            return 1;
-                        else
-                            return 2;
-                    }
-                    repete = true;
-                }
-            }
-
-            //DIAGONAL SECUNDÁRIA
-            repete = false;
-            comparar = matriz[1, 1];
-            for (int i = 0, y = 2; i <  2 * (matriz.GetLength(0) - 1); i += 2, y -= 2)
-            {
-                if (comparar == matriz[i, y])
-                {
-                    if (repete)
-                    {
-                        if (matriz[i, y] == " X")
-                            return 1;
-                        else
-                            return 2;
-                    }
-
-                    repete = true;
-                }
-            }
             return 0;
         }
     }
